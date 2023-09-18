66fb5d5e5948463bfa7f0d99361ce9bfcec29454
SabrinaSS <36011331+SabrinaSS@users.noreply.github.com>
2023-09-18 17:26:53 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { HashRouter as Router } from 'react-router-dom/cjs/react-router-dom.min';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+oot.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
